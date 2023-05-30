d79f4a12200454e42b4953d96eda6586fa561786
Rodrigo Ezequiel Miliano <rodrigomiliano16@gmail.com>
2023-05-30 14:31:37 -0300

    mejoras de consola en producto

--- src/main/java/comanda/service/jpa/ProductosService.java
+++ src/main/java/comanda/service/jpa/ProductosService.java
@@ -13,24 +13,39 @@ public class ProductosService implements IProductosService {
 
 	@Autowired
 	private ProductosRepository repoProductos;
-
+	
 	public List<Producto> buscarTodos() {
+		System.out.println("------------------------------------------------------------");
 		List<Producto> productos = repoProductos.findAll();
+		System.out.println("Listado de Productos: ");
 		productos.forEach(t -> {
 			System.out.println(t);
 		});
 		return repoProductos.findAll();
 	}
-
+		
 	public void guardar(Producto producto) {
+		System.out.println("------------------------------------------------------------");		
 		repoProductos.save(producto);
+		System.out.println("Guardando " + producto);
 	}
-
+	
 	public void eliminar(int idProducto) {
+		System.out.println("Eliminando registro: " + buscarProducto(idProducto));
 		repoProductos.deleteById(idProducto);
-	}
+	}	
 
 	public Optional<Producto> buscarProducto(int idProducto) {
-		return repoProductos.findById(idProducto);
-	}
+		System.out.println("------------------------------------------------------------");
+		Optional<Producto> optional = repoProductos.findById(idProducto);
+		if (optional.isPresent()) {
+			Producto u = optional.get();
+			System.out.println("Elegiste " + u);
+			return repoProductos.findById(idProducto);
+		} else {
+			System.out.println("------------------------------------------------------------");
+			System.out.println("No existe el Producto n° " + idProducto);
+		}
+		return null;
+	}	
 }
