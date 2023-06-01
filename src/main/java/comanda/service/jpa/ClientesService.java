8a2afbf78c5e9d55eacc582f368c1f524a7bcc98
Rodrigo Ezequiel Miliano <rodrigomiliano16@gmail.com>
2023-05-31 23:51:28 -0300

    mejoras de consola en cliente

--- src/main/java/comanda/service/jpa/ClientesService.java
+++ src/main/java/comanda/service/jpa/ClientesService.java
@@ -15,18 +15,38 @@ public class ClientesService implements IClientesService {
 	private ClientesRepository repoClientes;
 
 	public List<Cliente> buscarTodos() {
-		return repoClientes.findAll();
+		System.out.println("------------------------------------------------------------");
+		List<Cliente> clientes = repoClientes.findAll(); // consola de spring
+		System.out.println("Listado de Clientes: ");
+		clientes.forEach(t -> {
+			System.out.println(t);
+		});
+		return repoClientes.findAll(); // postman
 	}
-
-	public void guardar(Cliente Cliente) {
-		repoClientes.save(Cliente);
-	}
-
-	public void eliminar(int idCliente) {
-		repoClientes.deleteById(idCliente);
+		
+	public void guardar(Cliente cliente) {
+		System.out.println("------------------------------------------------------------");
+		repoClientes.save(cliente);
+		System.out.println("Guardando " + cliente);
 	}
 	
-	public Optional<Cliente> buscarCliente(int idCliente) {
-		return repoClientes.findById(idCliente);
+	public void eliminar(int idCliente) {
+		System.out.println("Eliminando registro: " + buscarCliente(idCliente));
+		repoClientes.deleteById(idCliente);
 	}
+		
+	public Optional<Cliente> buscarCliente(int idCliente) {
+		System.out.println("------------------------------------------------------------");
+		Optional<Cliente> optional = repoClientes.findById(idCliente);
+		if (optional.isPresent()) {
+			Cliente u = optional.get();
+			System.out.println("Elegiste " + u);
+			return repoClientes.findById(idCliente);
+		} else {
+			System.out.println("------------------------------------------------------------");
+			System.out.println("No existe el Cliente n° " + idCliente);
+		}
+		return null;
+	}	
+	
 }
