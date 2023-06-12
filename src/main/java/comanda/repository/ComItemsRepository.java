5392e3458d682f279ed30ab15eae7eb6f6548e6a
Rodrigo Ezequiel Miliano <rodrigomiliano16@gmail.com>
2023-06-11 22:41:48 -0300

    elimino comitems y precios

--- src/main/java/comanda/repository/ComItemsRepository.java
+++
@@ -1,8 +0,0 @@
-package comanda.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import comanda.entity.ComItem;
-
-public interface ComItemsRepository extends JpaRepository<ComItem, Integer> {
-
-}
