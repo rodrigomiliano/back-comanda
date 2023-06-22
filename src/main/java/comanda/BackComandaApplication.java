c46884461171d5d50154a4791ccbe28c01200671
Francisco José Montero <francisco.fmontero@mercadolibre.com>
2023-06-22 19:32:02 -0300

    Agregado de component scan para que tome los endpoints y modificaciones en el pom

--- src/main/java/comanda/BackComandaApplication.java
+++ src/main/java/comanda/BackComandaApplication.java
@@ -3,9 +3,11 @@ package comanda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"comanda.service.mapper"})
+@ComponentScan(basePackages = {"comanda.service.mapper", "comanda.service", "comanda.controller", "comanda.repository", "comanda.entity"})
+@Configuration
 public class BackComandaApplication {
 
 	public static void main(String[] args) {
