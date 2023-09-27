c473133040f07aad69a995644977d6728979dff5
Rodrigo Ezequiel Miliano <rodrigomiliano16@gmail.com>
2023-09-26 21:49:06 -0300

    estado actualizado

--- src/main/java/comanda/service/jpa/EstadosService.java
+++ src/main/java/comanda/service/jpa/EstadosService.java
@@ -15,18 +15,37 @@ public class EstadosService implements IEstadosService {
 	private EstadosRepository repoEstados;
 
 	public List<Estado> buscarTodos() {
-		return repoEstados.findAll();
+		System.out.println("------------------------------------------------------------");
+		List<Estado> estados = repoEstados.findAll(); // Esto lo ordena en la consola de spring
+		System.out.println("Listado de Estados: ");
+		estados.forEach(t -> {
+			System.out.println(t);
+		});
+		return repoEstados.findAll(); // postman
 	}
 
 	public void guardar(Estado estado) {
+		System.out.println("------------------------------------------------------------");
 		repoEstados.save(estado);
+		System.out.println("Guardando " + estado);
 	}
 
 	public void eliminar(int idEstado) {
+		System.out.println("Eliminando registro: " + buscarEstado(idEstado));
 		repoEstados.deleteById(idEstado);
 	}
 	
 	public Optional<Estado> buscarEstado(int idEstado) {
-		return repoEstados.findById(idEstado);
+		System.out.println("------------------------------------------------------------");
+		Optional<Estado> optional = repoEstados.findById(idEstado);
+		if (optional.isPresent()) {
+			Estado u = optional.get();
+			System.out.println("Elegiste " + u);
+			return repoEstados.findById(idEstado);
+		} else {
+			System.out.println("------------------------------------------------------------");
+			System.out.println("No existe el Estado n° " + idEstado);
+		}
+		return null;
 	}
 }
